af826a99b3665522ddb669cd9d0f8efbc71b2771
Vagner Oliveira <vagner@vnda.com.br>
2014-05-23 15:42:10 -0300

    Alterando Html para nova Veerssao

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-  $(function() {
-    $( "#headings .heading > a" ).on( "click" , function () {
-      var elm = $( this );
-      $( "#headings .heading" ).removeClass( "active" );
-      $( "#headings .heading ul" ).fadeOut();
-      elm.parent().addClass( "active" );
-      elm.parent().find( "ul" ).fadeIn();
-    });
-     $( "#headings .heading a" ).on( "click" , function () {
-      var elm = $( this );
-      $( "#headings a" ).removeClass( "active" );
-      elm.addClass( "active" );
-    });
-  });
+
