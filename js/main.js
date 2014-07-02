bf09957587123ac672edf287c43c35f053d2cbe0
Vagner Oliveira <vagner@vnda.com.br>
2014-07-02 11:55:45 -0300

    Atualizando Conteúdo

--- js/main.js
+++ js/main.js
@@ -4,5 +4,16 @@ $(function(){
 	    $( "select option:selected" ).each(function() {
 	      window.location.href = $( this ).val();
 	    });
-	  })
- })
+	  });
+	  $( window ).load(function(){
+		 	 $( ".tocify-wrapper li" ).each(function() {
+		    var texto = $( this ).find( "a" ).text();
+		    if( texto == "Liquid para Designers(filtros)" ) {
+		    	$( this ).find( "a" ).attr({
+		    		"href": "https://github.com/Shopify/liquid/wiki/Liquid-for-Designers",
+		    		"target":"_blank"
+		    	});
+		    }
+		  });
+	  });
+ });
