30a538a7987fca46f53de3d59f46bcf9dae6fd66
Vagner Oliveira <vagner@vnda.com.br>
2014-07-02 15:46:29 -0300

    Seção Template!

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(function(){
 		    var texto = $( this ).find( "a" ).text();
 		    if( texto == "Liquid para Designers(filtros)" ) {
 		    	$( this ).find( "a" ).attr({
-		    		"href": "https://github.com/Shopify/liquid/wiki/Liquid-for-Designers",
+		    		"href": "https://github.com/Shopify/liquid/wiki/Liquid-for-Designers#standard-filters",
 		    		"target":"_blank"
 		    	});
 		    }
