527706dfc2b1058b8c227340a1c11ddd2a4b6490
Vagner Oliveira <vagner@vnda.com.br>
2014-07-30 13:55:20 -0300

    Incluido dados para entrega/pagamento - link para github

--- js/main.js
+++ js/main.js
@@ -8,6 +8,12 @@ $(function(){
 		    		"target":"_blank"
 		    	});
 		    }
+		    if( texto == "Biblioteca Jquery para validar cpf e cnpj" ) {
+		    	$( this ).find( "a" ).attr({
+		    		"href": "https://gist.github.com/vagnerolliver/8e60807e5269162a58b7",
+		    		"target":"_blank"
+		    	});
+		    }
 		  });
 	  });
  });
