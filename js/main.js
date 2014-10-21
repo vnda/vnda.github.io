7df750eb117130a6cb0729400bfa5f3687e9545c
Vagner Oliveira <vagner@vnda.com.br>
2014-10-21 16:07:42 -0200

    Atualização Liquid for designers

--- js/main.js
+++ js/main.js
@@ -2,12 +2,6 @@ $(function(){
 	  $( window ).load(function(){
 		 	 $( ".tocify-wrapper li" ).each(function() {
 		    var texto = $( this ).find( "a" ).text();
-		    if( texto == "Liquid para Designers(filtros)" ) {
-		    	$( this ).find( "a" ).attr({
-		    		"href": "https://github.com/Shopify/liquid/wiki/Liquid-for-Designers#standard-filters",
-		    		"target":"_blank"
-		    	});
-		    }
 		    if( texto == "Biblioteca Jquery para validar cpf e cnpj" ) {
 		    	$( this ).find( "a" ).attr({
 		    		"href": "https://gist.github.com/vagnerolliver/8e60807e5269162a58b7",
